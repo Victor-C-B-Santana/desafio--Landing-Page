ad40ed9bc9b196d9c0a6b450e49efbf10cc25511
Victor <player.17112008@gmail.com>
2024-08-22 08:43:24 -0300

    Enviando o codigo completo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './pages/pages.css';
+
+import Menu from './components/menu/menu'
+import Footer from './components/footer/footer.js'
+
+import Page1 from './pages/page1.js';
+import Page2 from './pages/page2.js';
+import Page3 from './pages/page3.js';
+import Page4 from './pages/page4.js';
+import Page5 from './pages/page5.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Menu></Menu>
       </header>
+      
+      <Page1></Page1>
+      <Page2></Page2>
+      <Page3></Page3>
+      <Page4></Page4>
+      <Page5></Page5>
+
+      <Footer></Footer>
+
     </div>
   );
 }
